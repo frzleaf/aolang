3d765dac981ef0f1fb08f80fbb7c7366adc475d7
Thanh <qle@tibco.com>
2021-04-13 13:58:26 +0700

    Update ping

--- src/main/client.go
+++ src/main/client.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	ln, _ := net.ListenUDP("udp4", &net.UDPAddr{Port: 11260})
-	raddr := &net.UDPAddr{IP: []byte{255, 255, 255, 255}, Port: 6112}
+	raddr := &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 6112}
+	//raddr := &net.UDPAddr{IP: []byte{255, 255, 255, 255}, Port: 6112}
 	b := make([]byte, 128)
 	for {
 		n, _ := ln.Read(b)
