4175afe5ce9dc97ccab68ea76e9781e01aa37e82
Le Thanh <stash.leth@gmail.com>
2021-04-15 04:50:15 +0700

    finish

--- src/main/server.go
+++ src/main/server.go
@@ -4,86 +4,143 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	laddr := net.UDPAddr{
+
+	if len(os.Args) < 3 {
+		fmt.Println("Không điền đủ tham số, vui lòng nhập lại theo mẫu: ")
+		fmt.Println("lancraft 10.0.0.1 10.0.0.2")
+	}
+
+	lAddr := os.Args[1]
+	rAddr := os.Args[2]
+
+	for {
+		watchAndForward(lAddr, rAddr)
+	}
+}
+
+func watchAndForward(lAddr, rAddr string) {
+	lUdpaddr := net.UDPAddr{
 		Port: 6110,
-		IP:   net.ParseIP("10.8.0.2"),
+		IP:   net.ParseIP(lAddr),
 	}
 
-	l2addr := net.UDPAddr{
+	l2Udpaddr := net.UDPAddr{
 		Port: 6112,
-		IP:   net.ParseIP("10.8.0.2"),
+		IP:   net.ParseIP(lAddr),
 	}
-
-	//listen, err2 := net.ListenUDP("udp", &l2addr)
-	//if err2 != nil {
-	//	log.Fatal(err2)
-	//}
-	//defer listen.Close()
-	//go func() {
-	//	for {
-	//		var inputData []byte
-	//		udp, addr, err := listen.ReadFromUDP(inputData)
-	//		if err != nil {
-	//			fmt.Println(err)
-	//		} else {
-	//			fmt.Println(inputData[0:udp], " from ", addr)
-	//		}
-	//		time.Sleep(time.Second * 1)
-	//	}
-	//}()
 
 	raddr := net.UDPAddr{
 		Port: 6112,
-		IP:   net.ParseIP("10.8.0.6"),
+		IP:   net.ParseIP(rAddr),
 	}
-	ln, err := net.DialUDP("udp4", &laddr, &raddr)
+
+	scannerConn, err := net.DialUDP("udp4", &lUdpaddr, &raddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	defer scannerConn.Close()
 	buf := make([]byte, 1000)
-	//for {
-	_, err = ln.Write(
-		[]byte{0xf7, 0x2f, 0x10, 0x00, 0x50, 0x58, 0x33, 0x57, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-		//&raddr,
-	)
-	read, client, err := ln.ReadFromUDP(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(buf[0:read], " from ", client)
+	var scanCounter byte = 0
 
-	dial, err := net.DialUDP("udp4", nil, &l2addr)
-	//dial, err := net.DialUDP("udp4", &laddr, &l2addr)
-	if err != nil {
-		log.Println(err)
-	} else {
-		_, err = dial.Write(buf[0:read])
+	joinedGame := false
+	gameListConn, err := net.DialUDP("udp4", nil, &l2Udpaddr)
+	for {
+		_, err = scannerConn.Write(
+			[]byte{0xf7, 0x2f, 0x10, 0x00, 0x50, 0x58, 0x33, 0x57, 0x18, 0x00, 0x00, 0x00, scanCounter, 0x00, 0x00, 0x00},
+		)
+		scanCounter = scanCounter + 1
+		scannerConn.SetReadDeadline(time.Now().Add(time.Millisecond * 500))
+		read, client, err := scannerConn.ReadFromUDP(buf)
+		if err != nil {
+			continue
+		}
+
 		if err != nil {
 			log.Println(err)
+		} else {
+			fmt.Println("Host " + client.IP.String() + "     : " + string(buf[0:read]))
+			go func() {
+				defer gameListConn.Close()
+				for !joinedGame {
+					_, err = gameListConn.Write(buf[0:read])
+					time.Sleep(time.Second)
+				}
+			}()
+			break
 		}
 	}
-	tcpAddr := net.TCPAddr{
-		IP:   net.ParseIP("0.0.0.0"),
-		Port: 6112,
+
+	lTcpAddr := net.TCPAddr{
+		IP:   net.ParseIP(lAddr),
+		Port: 6110,
 	}
-	tcp, err := net.ListenTCP("tcp", &tcpAddr)
+	rTcpAddr := net.TCPAddr{
+		IP:   net.ParseIP(rAddr),
+		Port: 6110,
+	}
+	ltcp, err := net.ListenTCP("tcp", &lTcpAddr)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		acceptTCP, _ := tcp.AcceptTCP()
+		log.Fatal(err)
+	}
+	defer ltcp.Close()
+	tcpConn, err := ltcp.AcceptTCP()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Đã join game!")
+	joinedGame = true
+	outGame := false
+	go intermediate(tcpConn, rTcpAddr, func() {
+		fmt.Println("Đã out game")
+		outGame = true
+	})
+	for !outGame {
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+func intermediate(tcpConn net.Conn, rTcpAddr net.TCPAddr, onExit func()) {
+	rtcp, err := net.DialTCP("tcp", nil, &rTcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf1 := make([]byte, 1000)
+	buf2 := make([]byte, 1000)
+
+	go func() {
+		defer tcpConn.Close()
+		defer onExit()
 		for {
-			if i, err := acceptTCP.Read(buf); err != nil {
-				fmt.Println(err)
+			if r, err1 := tcpConn.Read(buf1); err1 != nil {
+				if err1.Error() != "EOF" {
+					fmt.Println("Có lỗi xảy ra:" + err1.Error())
+				}
+				break
 			} else {
-				fmt.Println("Receive", buf[0:i], acceptTCP.RemoteAddr())
+				rtcp.Write(buf1[0:r])
 			}
 		}
-	}
-	//time.Sleep(time.Second * 1)
-	//}
+	}()
 
+	go func() {
+		defer rtcp.Close()
+		for {
+			if r, err1 := rtcp.Read(buf2); err1 != nil {
+				if err1.Error() != "EOF" {
+					fmt.Println("Có lỗi xảy ra:" + err1.Error())
+				}
+				break
+			} else {
+				tcpConn.Write(buf2[0:r])
+			}
+		}
+	}()
 }
