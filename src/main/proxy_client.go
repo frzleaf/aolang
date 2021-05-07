dd25c6f55c4b6ef70d49deb8bcdfe49b0313f568
Le Thanh <stash.leth@gmail.com>
2021-05-07 18:00:33 +0700

    target server by stdin

--- src/main/proxy_client.go
+++ src/main/proxy_client.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 	"proxy"
+	"strings"
 )
 
 func main() {
 	client := proxy.NewClient()
-	client.Run(os.Args[1])
+
+	var hostAddr string
+	if len(os.Args) < 2 {
+		fmt.Println("Không tìm thấy host, vui lòng nhập địa chỉ host và port:")
+		fmt.Println("Ví dụ: 192.168.0.1:9999 hoặc lancraft.com:9999")
+
+		reader := bufio.NewReader(os.Stdin)
+		hostAddr, _ = reader.ReadString('\n')
+		hostAddr = strings.TrimSpace(hostAddr)
+	} else {
+		hostAddr = os.Args[1]
+	}
+
+	client.Run(hostAddr)
 }
