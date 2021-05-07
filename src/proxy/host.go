944c3275cdac2e932ba24efda6ee1916c266ae49
Le Thanh <stash.leth@gmail.com>
2021-05-07 17:37:23 +0700

    concurrent connection map

--- src/proxy/host.go
+++ src/proxy/host.go
@@ -6,11 +6,13 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 type Host struct {
 	hostConn          map[int]net.Conn // game connections
+	connLock          sync.Mutex       // support concurrent map
 	hostPort          int              // local host port
 	guestCon          net.Conn         // guest player connection
 	sConn             net.Conn         // server connection
@@ -31,6 +33,7 @@ func NewHost() *Host {
 		connectedHostPort: -1,
 		hostPort:          -1,
 		monitor:           &Monitor{},
+		connLock:          sync.Mutex{},
 	}
 }
 
@@ -107,7 +110,8 @@ func (c *Host) OpenProxyHost() (err error) {
 				}
 				targetHost := c.connectedHost
 				_, _ = c.sConn.Write(NewInformPacket(c.id, targetHost, []byte(CommandExitGame)).ToBytes())
-				LOG.Info("Game joined to:", c.guestCon.LocalAddr())
+				time.Sleep(time.Millisecond * 500)
+				LOG.Info("Game joined to: %v (%v)", targetHost, c.guestCon.LocalAddr())
 				buf := make([]byte, 1000)
 				for {
 					if read, err2 := c.guestCon.Read(buf); err2 != nil {
@@ -150,7 +154,7 @@ func (h *Host) OpenBroadCastListener() error {
 			if h.isHost {
 				continue
 			}
-			LOG.Infof("Receive broadcast message, len = %v", len(buf[0:read]))
+			LOG.Debugf("Receive broadcast message, len = %v", len(buf[0:read]))
 			h.sConn.Write(NewPacket(
 				PackageTypeBroadCast,
 				h.id,
@@ -211,7 +215,7 @@ func (h *Host) PrepareNewGameConnection(srcId int) (gConn net.Conn, err error) {
 	if err != nil {
 		return
 	}
-	h.hostConn[srcId] = gConn
+	h.addConnection(srcId, gConn)
 	go h.DataHostToServer(srcId)
 	return
 }
@@ -231,8 +235,7 @@ func (h *Host) DataHostToServer(srcId int) {
 				continue
 			}
 			h.monitor.onError(err)
-			hConn.Close()
-			delete(h.hostConn, srcId)
+			h.removeConnection(srcId)
 			break
 		} else {
 			if _, err = h.sConn.Write(NewPacket(PackageTypeToGuest, h.id, srcId, buf[:read]).ToBytes()); err != nil {
@@ -240,6 +243,21 @@ func (h *Host) DataHostToServer(srcId int) {
 			}
 		}
 	}
+}
+
+func (h *Host) addConnection(srcId int, newConn net.Conn) {
+	h.connLock.Lock()
+	h.hostConn[srcId] = newConn
+	h.connLock.Unlock()
+}
+
+func (h *Host) removeConnection(srcId int) {
+	h.connLock.Lock()
+	if h.hostConn[srcId] != nil {
+		h.hostConn[srcId].Close()
+		delete(h.hostConn, srcId)
+	}
+	h.connLock.Unlock()
 }
 
 func (h *Host) start(serverAddr string) (err error) {
@@ -250,7 +268,6 @@ func (h *Host) start(serverAddr string) (err error) {
 	// Set up broadcast watchClient address
 	h.gameConfig.internalRemoteIp = strings.Split(h.sConn.LocalAddr().String(), ":")[0]
 	go h.OpenBroadCastListener()
-	go h.OpenProxyHost()
 	return h.watchGameData()
 }
 
@@ -280,7 +297,7 @@ func (h *Host) serverToGame(data []byte) (err error) {
 
 // TODO consider using event emitter
 func (h *Host) gameReceiveMessage(packet *Packet) (err error) {
-	LOG.Infof("Receive msg from %v len %v\n", packet.SrcAddr(), len(packet.Data()))
+	LOG.Debugf("Receive msg from %v len %v\n", packet.SrcAddr(), len(packet.Data()))
 
 	if packet.src == ServerConnectorID {
 		h.resolveCommandFromServer(packet)
@@ -325,10 +342,7 @@ func (h *Host) resolveCommandFromServer(packet *Packet) {
 			}
 		}
 	case CommandExitGame:
-		if h.hostConn[packet.SrcAddr()] != nil {
-			h.hostConn[packet.SrcAddr()].Close()
-			delete(h.hostConn, packet.SrcAddr())
-		}
+		h.removeConnection(packet.SrcAddr())
 	default:
 		LOG.Warn("Invalid command: ", serverCmd)
 	}
