ca2f8eccf43bfd5f1e917a8b26a19501d0fc2d30
Le Thanh <stash.leth@gmail.com>
2021-07-22 11:26:48 +0700

    Add server starting caption

--- src/main/proxy_server.go
+++ src/main/proxy_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 	"proxy"
 )
@@ -8,6 +9,10 @@ import (
 func main() {
 
 	server := proxy.NewServer()
-	server.Start(os.Args[1])
+	if len(os.Args) > 1 {
+		server.Start(os.Args[1])
+	} else {
+		log.Fatalln("Please run with args, example: lancraft_server 0.0.0.0:9999")
+	}
 
 }
